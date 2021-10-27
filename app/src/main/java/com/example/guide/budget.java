a3da9b1c59533488fbccacdd2c1323b66da0c6a5
Chandran Arani <arani.chandran@edu.esiee.fr>
2021-10-27 20:12:06 +0200

    Ajout des différentes classes

---
+++ app/src/main/java/com/example/guide/budget.java
@@ -0,0 +1,14 @@
+package com.example.guide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class budget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget);
+    }
+}
--- app/src/main/java/com/example/guide/page2.java
+++ app/src/main/java/com/example/guide/page2.java
@@ -2,7 +2,12 @@ package com.example.guide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class page2 extends AppCompatActivity {
 
@@ -10,5 +15,52 @@ public class page2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+        Button mcuisine = findViewById(R.id.button3);
+        Button mbudget = findViewById(R.id.button6);
+        Button mprix = findViewById(R.id.button7);
+        Button mvisiter = findViewById(R.id.button8);
+        Button magenda = findViewById(R.id.button9);
+
+
+        mcuisine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(page2.this, Cuisine.class);
+                startActivity(intent);
+            }
+        });
+
+        mbudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(page2.this, budget.class);
+                startActivity(intent);
+            }
+        });
+
+        mprix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(page2.this, prix.class);
+                startActivity(intent);
+            }
+        });
+        mvisiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(page2.this, visite.class);
+                startActivity(intent);
+            }
+        });
+        magenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(page2.this, agenda.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
+
+
