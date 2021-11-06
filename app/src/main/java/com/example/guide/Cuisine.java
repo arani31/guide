922feab513a37edb9b6fef15f0dc1ff39d0fcec6
Chandran Arani <arani.chandran@edu.esiee.fr>
2021-11-06 10:39:42 +0100

    Ajout des boutons Recette et Restaurant

--- app/src/main/java/com/example/guide/Cuisine.java
+++ app/src/main/java/com/example/guide/Cuisine.java
@@ -2,13 +2,38 @@ package com.example.guide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Cuisine extends AppCompatActivity {
-
+    ImageButton mrecette ;
+    ImageButton mrestaurant;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuisine);
+       mrecette = (ImageButton) findViewById(R.id.recette);
+       mrestaurant =(ImageButton)findViewById(R.id.restuarant) ;
+
+       mrecette.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cuisine.this, recette.class);
+                startActivity(intent);
+            }
+        });
+        mrestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cuisine.this, recette.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
