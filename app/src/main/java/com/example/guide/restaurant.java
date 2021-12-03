8c393f41adae26de27f34c49c0db3a0c64266dfe
Chandran Arani <arani.chandran@edu.esiee.fr>
2021-12-03 14:50:25 +0100

    lieux a visiter

--- app/src/main/java/com/example/guide/restaurant.java
+++ app/src/main/java/com/example/guide/restaurant.java
@@ -10,27 +10,5 @@ import java.util.ArrayList;
 
 public class restaurant extends AppCompatActivity {
 
-        private ArrayList<lieux> lieuxList;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant);
-        lieuxList = new ArrayList<>();
-
-        Resources res =getResources();
-        String [] allLieux = res.getStringArray(R.array.lieux );
-        String [] alladress = res.getStringArray(R.array.adresse);
-        pupoluatelieuxList (allLieux ,alladress);
-
-         LieuxAdapter adapter =new LieuxAdapter(getApplicationContext(), R.layout.list_item,lieuxList);
-    ListView listview =findViewById(R.id.lieux_list);
-    listview.setAdapter(adapter);
-    }
-    public void pupoluatelieuxList(String[] lieux, String [] adresse){
-
-       lieuxList.add(new lieux( lieux[0], adresse[0],R.drawable.ocean ));
-    }
 
 }
